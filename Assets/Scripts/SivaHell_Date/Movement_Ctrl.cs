69828f7094382eb39f24b624e46719bed3c99b42
Siva_hell <kk3398649@gmail.com>
2025-01-25 12:26:29 +0800

    Na.SivaHell.Day.1.25.t.12.16

--- Assets/Scripts/SivaHell_Date/Movement_Ctrl.cs
+++ Assets/Scripts/SivaHell_Date/Movement_Ctrl.cs
@@ -3,37 +3,55 @@ namespace Bubble
 {
     public class Movement_Ctrl : MonoBehaviour
     {
-        bool Player_Is_Move = false;
+        bool Player_Is_Move = false, Player_Is_Dash = false;
         Vector3 Move_power = new(0, 0, 0), Null_vec3 = new (0, 0, 0);
+        [SerializeField] SpriteRenderer Player_Image;
+
         void Start()
         {
-            Debug.Log("MoveCtrl is Start.");
+            if (Player_Image == null) Player_Image = this.gameObject.GetComponent<SpriteRenderer>();
+            Debug.Log(" *MoveCtrl is Start.\nPlayer Input (D) => +,\n(A) => -,\n(Space) => MoveUp,\n(W) => Add Bubble,\n(Shift) => Dash,(Shift + D or A) => Big Dash");
+            Player_Move();
         }
 
         void Update()
         {
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.LeftArrow))/*+z*/
             {
                 Move_power.z++;
                 Player_Is_Move = true;
             }
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.RightArrow))/*-z*/
             {
                 Move_power.z--;
                 Player_Is_Move = true;
             }
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.Space))/*MoveUp*/
             {
                 Move_power.y++;
                 Player_Is_Move = true;
             }
-            if (Player_Is_Move) Player_Is_Move = Player_Move();
+            if (Input.GetKeyDown(KeyCode.LeftShift)) Player_Is_Dash = true;/*Dash*/
+            /*Move return to odrer.*/
+            if (Player_Is_Move || Player_Is_Dash) Player_Is_Dash = Player_Is_Move = Player_Move();
         }
         private bool Player_Move()
         {
-            Debug.Log($" *PlayerMove_Func();\n{Move_power}");
-            transform.position += Move_power * Time.deltaTime;
-            Move_power = Null_vec3;
+            //Debug.Log($" *PlayerMove_Func();\n{Move_power},{Player_Is_Dash}");/*Return this.func() is work.*/
+            switch (Move_power.z)/*Update Player Image.*/
+            {
+                case 1:
+                    Player_Image.flipX = false;
+                    break;
+                case -1:
+                    Player_Image.flipX = true;
+                    break;
+            }
+            if (Player_Is_Dash)/*Add dash power*/
+                if (Move_power.z != 0) Move_power.z *= 300;/*Player is on Move*/
+                else Move_power.z += Player_Image.flipX ? -150f : 150f;/*Player just Input.(Dash)*/
+            transform.position += Move_power * Time.deltaTime;/*暫時的*/
+            Move_power = Null_vec3;/*Reset Vec3 date.*/
             return false;
         }
     }
---
+++ Assets/Scripts/SivaHell_Date/Unfriendly_Bot_0.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unfriendly_Bot_0 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
