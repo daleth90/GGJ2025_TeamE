2abe7c96d62ecfcaf822506b783fac867619d420
Siva_hell <kk3398649@gmail.com>
2025-01-25 16:52:59 +0800

    NA

--- Assets/Scripts/SivaHell_Date/Player_Input_Ctrl.cs
+++ Assets/Scripts/SivaHell_Date/Player_Input_Ctrl.cs
@@ -37,6 +37,7 @@ namespace Bubble
                 if (Input.GetKeyDown(KeyCode.LeftShift)) Player_Is_Dash = true;/*Dash*/
                 if (Player_Is_Move || Player_Is_Dash)
                     Player_Is_Dash = Player_Is_Move = movement_Ctrl.Player_Move(Player_Image, Player_Is_Dash);
+                else movement_Ctrl.Player_Move();
                 if (Input.GetKeyDown(KeyCode.W))
                 {
                     Debug.Log("Add Bubble.");
