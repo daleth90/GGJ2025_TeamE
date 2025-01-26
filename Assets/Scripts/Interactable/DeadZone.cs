f33369f0b9104461f408d494144c07a7665db659
jiajin0329 <98139948+jiajin0329@users.noreply.github.com>
2025-01-26 11:09:27 +0800

    dead fx.

--- Assets/Scripts/Interactable/DeadZone.cs
+++ Assets/Scripts/Interactable/DeadZone.cs
@@ -1,12 +1,21 @@
+using Physalia;
 using UnityEngine;
 
 namespace Bubble
 {
     public class DeadZone : MonoBehaviour, IInteractable
     {
+        private IVfxManager _ivfxManager;
+
+        private void Awake()
+        {
+            _ivfxManager = ServiceLocator.Resolve<IVfxManager>();
+        }
+
         public void Interact(PlayerStatus playerStatus = null)
         {
             GameSystemModel.instance.GameFail();
+            _ivfxManager.PlayOneShot("CFXR3 Hit Misc A", playerStatus.transform.position);
         }
     }
 }
